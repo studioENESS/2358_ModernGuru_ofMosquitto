ec9364f756a076a16fb3601f5740aae9ae593d2b
Bruno Herfst <bruno.herfst@gmail.com>
2024-07-30 11:02:00 +1000

    Create setIP.sh

---
+++ scripts/setIP.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Add the Static IP template
+
+if grep -Fxq "interface wlan0" /etc/dhcpcd.conf
+then
+    echo "wlan0 IP allready defined. Update IP manually or update this script."
+    exit 1
+else
+    # Prompt user to enter the last number of the IP address
+    read -p "Enter last number of static IP for wlan0: " last_number
+    last_number=${last_number:-1}
+
+    # Validate if the entered number is within valid range (1-254)
+    if ((last_number < 30)) || ((last_number > 254)); then
+        echo "Invalid number. Please enter a number between 30 and 254."
+        exit 1
+    fi
+
+    echo "\n" >> /etc/dhcpcd.conf
+    echo "interface wlan0" >> /etc/dhcpcd.conf
+    echo "ip_address=192.168.0.${last_number}/24" >> /etc/dhcpcd.conf
+    echo "routers=192.168.0.255" >> /etc/dhcpcd.conf
+    echo "domain_name_servers=192.168.0.255" >> /etc/dhcpcd.conf
+fi
+
+echo "Done!"
